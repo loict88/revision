03106ead7df7a820cb009f09db29b373f170ad07
loict88 <loic88new@hotmail.fr>
2021-01-11 17:07:16 +0100

    verifs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,9 +8,7 @@ import './styles.css';
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-  </div>
+   
 );
 
 // == Export
